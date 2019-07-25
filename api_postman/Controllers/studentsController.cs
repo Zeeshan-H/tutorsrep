2f8d76ce5b6b6d5c70d7ae956c08de43a485ba59
Zeeshan-H <zeeshanh184@gmail.com>
2019-07-26 03:06:14 +0500

    Final API

---
+++ api_postman/Controllers/studentsController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace api_postman.Controllers
+{
+
+         [Authoriza]
+    public class studentsController : ApiController
+    {
+
+
+   
+       [HttpGet]
+       [Route("api/students")]
+    
+        public IEnumerable<tbl_student> Get()
+        {
+            using (tutorEntities1 entities = new tutorEntities1())
+            {
+
+                return entities.tbl_student.ToList();
+
+            }
+
+
+        }
+
+          public tbl_student Get(int id)
+          {
+              using (tutorEntities1 entities = new tutorEntities1())
+              {
+
+                  return entities.tbl_student.FirstOrDefault(t => t.id == id);
+
+              }
+
+
+          }
+
+          [HttpPost]
+          [Route("api/students")]
+
+          public HttpResponseMessage Post([FromBody] tbl_student tutor)
+          {
+              try
+              {
+                  using (tutorEntities1 entities = new tutorEntities1())
+                  {
+
+                      entities.tbl_student.Add(tutor);
+                      entities.SaveChanges();
+                      var message = Request.CreateResponse(HttpStatusCode.Created, tutor);
+                      message.Headers.Location = new Uri(Request.RequestUri + tutor.id.ToString());
+                      return message;
+                  }
+              }
+              catch (Exception e)
+              {
+                  return Request.CreateResponse(HttpStatusCode.BadRequest, e);
+
+              }
+
+          }
+        [HttpDelete]
+
+
+
+          public HttpResponseMessage Delete(int id)
+          {
+
+              try
+              {
+
+
+                  using (tutorEntities1 entities = new tutorEntities1())
+                  {
+
+                      var entity = entities.tbl_student.Remove(entities.tbl_student.FirstOrDefault(t => t.id == id));
+
+
+                      if (entity == null)
+                      {
+
+                          return Request.CreateResponse(HttpStatusCode.NotFound, "Tutor with ID=" + id.ToString() + "not found to be deleted");
+                      }
+                      else
+                      {
+                          entities.tbl_student.Remove(entity);
+                          entities.SaveChanges();
+                          return Request.CreateResponse(HttpStatusCode.OK);
+                      }
+
+
+                  }
+              }
+              catch (Exception e)
+              {
+                  return Request.CreateErrorResponse(HttpStatusCode.BadRequest, e);
+
+              }
+
+          }
+
+
+        [HttpPut]
+
+          public HttpResponseMessage Put(int id, [FromBody]tbl_student tutor)
+          {
+
+              try
+              {
+
+                  using (tutorEntities1 entities = new tutorEntities1())
+                  {
+
+                      var entity = entities.tbl_student.FirstOrDefault(t => t.id == id);
+
+                      if (entity == null)
+                      {
+
+
+                          return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Tutor with ID=" + id.ToString() + "not found to be updated");
+                      }
+
+
+                      else
+                      {
+
+                          entity.id = tutor.id;
+                          entity.Email = tutor.Email;
+                          entity.Name = tutor.Name;
+                          entity.City = tutor.City;
+                          entity.Address = tutor.Address;
+                          entity.Description = tutor.Description;
+                          entity.Dob = tutor.Dob;
+                          entity.Phone = tutor.Phone;
+                          entity.Class = tutor.Class;
+                          entity.Subjects = tutor.Subjects;
+
+                          entities.SaveChanges();
+                          return Request.CreateResponse(HttpStatusCode.OK);
+                      }
+                  }
+              }
+
+              catch (Exception e)
+              {
+                  return Request.CreateErrorResponse(HttpStatusCode.BadRequest, e);
+
+              }
+          }
+
+    
+    
+    }
+}
---
+++ api_postman/Controllers/tutorsController.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+
+namespace api_postman.Controllers
+{
+    
+    
+    public class tutorsController : ApiController
+    {
+    
+  
+        [AllowAnonymous]
+ 
+        [HttpGet]
+        [Route("api/tutors")]
+
+
+        public IEnumerable<tbl_tutor> Get()
+        {
+            using (tutorEntities1 entities = new tutorEntities1())
+            {
+
+                return entities.tbl_tutor.ToList();
+
+            }
+            
+
+       
+
+             
+    }
+
+         
+
+          public tbl_tutor Get(int id)
+          {
+              using (tutorEntities1 entities = new tutorEntities1())
+              {
+
+                  return entities.tbl_tutor.FirstOrDefault(t => t.ID == id);
+
+              }
+
+
+          }
+           
+            
+            
+            
+       [AllowAnonymous]
+        [HttpPost]
+            [Route("api/tutors")]
+
+
+          public HttpResponseMessage Post([FromBody] tbl_tutor tutor)
+          {
+              try
+              {
+                  using (tutorEntities1 entities = new tutorEntities1())
+                  {
+
+                      entities.tbl_tutor.Add(tutor);
+                      entities.SaveChanges();
+                      var message = Request.CreateResponse(HttpStatusCode.Created, tutor);
+                      message.Headers.Location = new Uri(Request.RequestUri + tutor.ID.ToString());
+                      return message;
+                  }
+              }
+              catch (Exception e)
+              {
+                  return Request.CreateResponse(HttpStatusCode.BadRequest, e);
+
+              }
+
+          }
+
+        
+        [AllowAnonymous]
+      [HttpDelete]
+
+          public HttpResponseMessage Delete(int id)
+          {
+
+              try
+              {
+
+
+                  using (tutorEntities1 entities = new tutorEntities1())
+                  {
+
+                      var entity = entities.tbl_tutor.Remove(entities.tbl_tutor.FirstOrDefault(t => t.ID == id));
+
+
+                      if (entity == null)
+                      {
+
+                          return Request.CreateResponse(HttpStatusCode.NotFound, "Tutor with ID=" + id.ToString() + "not found to be deleted");
+                      }
+                      else
+                      {
+                          entities.tbl_tutor.Remove(entity);
+                          entities.SaveChanges();
+                          return Request.CreateResponse(HttpStatusCode.OK);
+                      }
+
+
+                  }
+              }
+              catch (Exception e)
+              {
+                  return Request.CreateErrorResponse(HttpStatusCode.BadRequest, e);
+
+              }
+
+          }
+
+        [AllowAnonymous]
+        [HttpPut]
+
+          public HttpResponseMessage Put(int id, [FromBody]tbl_tutor tutor)
+          {
+
+              try
+              {
+
+                  using (tutorEntities1 entities = new tutorEntities1())
+                  {
+
+                      var entity = entities.tbl_tutor.FirstOrDefault(t => t.ID == id);
+
+                      if (entity == null)
+                      {
+
+
+                          return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Tutor with ID=" + id.ToString() + "not found to be updated");
+                      }
+
+
+                      else
+                      {
+
+                          entity.ID = tutor.ID;
+                          entity.City = tutor.City;
+                          entity.Email = tutor.Email;
+                          entity.Name = tutor.Name;
+                          entity.Qualified = tutor.Qualified;
+                          entity.Gender = tutor.Gender;
+                          entity.Phone = tutor.Phone;
+                          entity.Classes = tutor.Classes;
+                          entity.Address = tutor.Address;
+                          entity.Available = tutor.Available;
+                          entity.Dob = tutor.Dob;
+                          entity.Experience = tutor.Experience;
+                          entity.Subjects = tutor.Subjects;
+                          entity.Description = tutor.Description;
+
+
+                          entities.SaveChanges();
+                          return Request.CreateResponse(HttpStatusCode.OK);
+                      }
+                  }
+              }
+
+              catch (Exception e)
+              {
+                  return Request.CreateErrorResponse(HttpStatusCode.BadRequest, e);
+
+              }
+          }
+
+ 
+    }
+}
